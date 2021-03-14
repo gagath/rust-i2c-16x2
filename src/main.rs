8600257abf6f0f594dbe5812d66d28ab51b665e5
Cristian Rojas <injcristianrojas@gmail.com>
2021-03-14 20:54:59 -0300

    Step separation

--- src/main.rs
+++ src/main.rs
@@ -9,20 +9,24 @@ use std::time::Duration;
 
 fn main() {
     let config = ScreenConfig::default();
-    let mut screen = Screen::new(config, "/dev/i2c-1", 0x3f).expect("Could not init device");
+    let mut screen = Screen::new(config, "/dev/i2c-1", 0x27).expect("Could not init device");
 
     println!("init");
     screen.init().unwrap();
+
     println!("off");
     screen.set_backlight(false).unwrap();
     thread::sleep(Duration::from_secs(1));
+    
     println!("on");
     screen.set_backlight(true).unwrap();
     thread::sleep(Duration::from_secs(1));
+    
     println!("show some text");
     screen.display("Hello Rust!", 1, 0).unwrap();
     screen.display("Fuck yeah :)", 2, 0).unwrap();
     thread::sleep(Duration::from_secs(5));
+    
     println!("off");
     screen.set_backlight(false).unwrap();
     thread::sleep(Duration::from_secs(1));
